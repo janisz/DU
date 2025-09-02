61cd7f5664d0adb11e06ff27ced4260687e831f0
Tomasz Janiszewski <janiszt@gmail.com>
2025-09-02 21:29:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -126,7 +126,7 @@ func prepareNewActs(old *oldApi.Client) ([]twitter.CreateTweetRequest, []func() 
 
 	var newActs []twitter.CreateTweetRequest
 	var summaries []func() (string, error)
-	for i := 0; i < 1; i++ {
+	for i := 0; i < 3; i++ {
 		lastTweetedId++
 
 		tweetText := getTweetText(year, 0, lastTweetedId)
